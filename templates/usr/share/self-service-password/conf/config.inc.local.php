a449597eb23b1189125240bc2a7f34c3aa93999b
Mathieu Jourdan <m.jourdan@rennesmetropole.fr>
2021-06-23 09:39:58 +0200

    [ssp] add quotation marks in template

--- templates/usr/share/self-service-password/conf/config.inc.local.php
+++ templates/usr/share/self-service-password/conf/config.inc.local.php
@@ -12,22 +12,22 @@ $debug = {{ ldaptoolbox_ssp_debug }};
 
 # LDAP Settings
 {% for key, value in ldaptoolbox_ssp_ldap_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 # Local password policy
 {% for key, value in ldaptoolbox_ssp_local_password_policy_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 # Mail settings
 {% for key, value in ldaptoolbox_ssp_mail_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 # Customization
 {% for key, value in ldaptoolbox_ssp_customization_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 
@@ -36,17 +36,17 @@ ${{ key }} = {{ value }};
 
 # Reset by mail
 {% for key, value in ldaptoolbox_ssp_reset_by_mail_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 # Reset by sms
 {% for key, value in ldaptoolbox_ssp_reset_by_sms_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 # Reset by question
 {% for key, value in ldaptoolbox_ssp_reset_by_question_settings.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 
@@ -54,7 +54,7 @@ ${{ key }} = {{ value }};
 # --------------------
 
 {% for key, value in ldaptoolbox_ssp_overrides.items() %}
-${{ key }} = {{ value }};
+${{ key }} = "{{ value }}";
 {% endfor %}
 
 
